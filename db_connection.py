072e8c66b4ec7f6389a03368aec10be395c6c747
Aparna V <aparnavellingiri267@gmail.com>
2026-03-24 13:41:44 +0530

    Add get_connection function for database access
    Encapsulate database connection creation in a function.

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
-engine = create_engine(
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-)
+def get_connection():
+    engine = create_engine(
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
+    return engine
